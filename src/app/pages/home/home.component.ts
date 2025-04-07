86893655e8c2cb68f6467e2c4655686436a7febd
dhani-dev <dhani.dev22@gmail.com>
2025-04-07 16:19:42 -0500

    Implementation of AfterViewInit in gallery

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, AfterViewInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import Swal from 'sweetalert2';
 import { User } from '../../shared/interfaces/user.interface';
@@ -9,21 +9,33 @@ import { User } from '../../shared/interfaces/user.interface';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit, OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit{
 
   followers = 15;
   authService;
   username = 'Bienvenido';
+  publicaciones = 0;
 
-  constructor(authService:AuthService){
+  @ViewChild('gallery') galleryRef!: ElementRef; //Obtiene una referencia al contenedor de imágenes
+
+  constructor(
+    authService: AuthService,
+    private cdr: ChangeDetectorRef 
+  ) {
     this.authService = authService;
-    console.log('Este es el constructor');
   }
   
   ngOnInit(): void {
     console.log('Este es OnInit');
     const user = this.authService.getUser();
     this.username = user? user.name!:'Bienvenido';
+  }
+
+  ngAfterViewInit(): void { //Asegura que las imagenes esten cargadas en el DOM
+    const images = this.galleryRef.nativeElement.querySelectorAll('img');
+    this.publicaciones = images.length;
+    this.cdr.detectChanges();
+    console.log('Este es AfterView');
   }
   
   ngOnDestroy(): void {
